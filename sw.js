e7ba71798bd227302ddf9a21b6567ec2132bd6be
lildaypay <lildaypay@gmail.com>
2016-09-25 22:34:30 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,23 +2,10 @@ console.log('Started', self);
  self.addEventListener('install', function(event) { self.skipWaiting(); console.log('Installed', event); }); 
  self.addEventListener('activate', function(event) { console.log('Activated', event); }); 
 // Register event listener for the 'push' event.
-
-/*
-
- self.addEventListener('push', function(event) {
-	    var payload = event.data ? event.data.text() : 'no payload';
-event.waitUntil(
  
-    self.registration.showNotification('Una Nuova NOtifica', {
-      body: payload,
-    })
-  );
-
-});  */
-
  self.addEventListener('push', function(event) {	
 event.waitUntil(  self.registration.pushManager.getSubscription().then(function(subscription) { var endp_min =subscription.endpoint.split("/").slice(-1);console.log("got subscription id: ", endp_min[0]);
- fetch('http://socialanime.it/endopoint.php?endpoint='+ endp_min[0]).then(function(response) {
+ fetch('https://socialanime.it/endopoint.php?endpoint='+ endp_min[0]).then(function(response) {
         if (response.status !== 200) {
             console.log('Problem. Status Code: ' + response.status);  
             throw new Error();  
@@ -38,6 +25,8 @@ event.waitUntil(  self.registration.pushManager.getSubscription().then(function(
   );
 
 }); 
+
+
 
 
 self.addEventListener('notificationclick', function(event) {  
@@ -64,5 +53,4 @@ self.addEventListener('notificationclick', function(event) {
     })
   );
 });
-
 
